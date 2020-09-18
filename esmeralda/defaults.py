74bbcfd5786becdc20496ba9b009b99150502d54
doubleO8 <wb008@hdm-stuttgart.de>
2020-09-18 08:27:06 +0200

    more configuration parameters

--- esmeralda/defaults.py
+++ esmeralda/defaults.py
@@ -8,7 +8,10 @@ ESMERALDA_CONFIG = dict(
         os.environ.get("HOME", ''), "ansible-playbooks"),
     ansible_playbook_binary="/usr/bin/ansible-playbook",
     amqp_port = 5672,
-    dispatch_queue="esmeralda_request"
+    dispatch_queue="esmeralda_request",
+    inventory='etc/00-merged-dummy',
+    playbook='information-dumping.yml',
+    ansible_config='etc/ansible.cfg'
 )
 
 PREFIX = 'ESMERALDA_'
